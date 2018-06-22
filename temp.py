46b8737f8f003572c830307b2d4288d0a9b55084
huyhoang <huyhoang1996ha@gmail.com>
2018-06-22 09:33:00 +0700

    clean project

--- temp.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import datetime 
-os.system('python server.py')
-t1 = datetime.now()
-# receive a packet
-while (datetime.now()-t1).seconds <= 4:
-	pass
-# os.system('python client.py')
